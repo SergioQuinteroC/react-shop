95a4d473f7203e17ca38677807f46c5bb585cebb
SergioQuinteroC <sergio.quinteroc@udea.edu.co>
2022-12-30 16:47:06 -0500

    add alias and fix img bug

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import "../styles/ProductItem.scss";
+import "@styles/ProductItem.scss";
+
+import bt_add_to_cart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
   return (
@@ -14,7 +16,7 @@ const ProductItem = () => {
           <p>Bike</p>
         </div>
         <figure>
-          <img src="./icons/bt_add_to_cart.svg" alt="" />
+          <img src={bt_add_to_cart} alt="" />
         </figure>
       </div>
     </div>
